ae39949a811215ca54016e1bbdcb5c3dd91e8447
Christopher Bare <christopher.bare@volparahealth.com>
2024-09-06 19:31:13 +1200

    regex fixes for missed items

--- tests/test_deidentify.py
+++ tests/test_deidentify.py
@@ -81,6 +81,51 @@ def test_DDDD_DDDD():
     assert "D'Angelo" not in deidentified_text
 
 
+def test_IDs():
+    text = """
+    Patient Name: Abigail Larsen
+    Patient ID: 7590-123-987
+    PATIENT_ACCOUNT_NUMBER:  7590-123-987
+    Acct: HT1234-7890
+    Acct: ZX123-123
+
+    This is innocent text, do not delete.
+    """
+    include_map, exclude_map, data_tracker = philter.detect_phi(text, patterns=filters)
+    deidentified_text = philter.transform_text_asterisk(text, include_map)
+
+    assert "HT1234" not in deidentified_text
+    assert "7890" not in deidentified_text
+    assert "7590" not in deidentified_text
+    assert "123" not in deidentified_text
+    assert "ZX123" not in deidentified_text
+    assert "987" not in deidentified_text
+    assert "Abigail" not in deidentified_text
+    assert "Larsen" not in deidentified_text
+    assert "This is innocent text, do not delete." in deidentified_text
+
+
+def test_directions_in_addresses():
+    text = """
+    Patient: Faithe Fraser
+    Address 1: 1234 W 45TH STREET S REDONDO BEACH CA 90277 US
+    Address 2: 1234 NW 45TH STREET S REDONDO BEACH CA 90277 US
+    Address 3: 1914 3rd Ave N, Seattle WA, 98019 USA
+    Address 4: 1600 Pennsylvania Ave NW, Washington, DC 20500
+
+    Not personally identifying. Totally OK text.
+    """
+
+    include_map, exclude_map, data_tracker = philter.detect_phi(text, patterns=filters)
+    deidentified_text = philter.transform_text_asterisk(text, include_map)
+
+    assert "1234 W 45TH STREET" not in deidentified_text
+    assert "1234 NW 45TH STREET" not in deidentified_text
+    assert "1914 3rd Ave N" not in deidentified_text
+    assert "1600 Pennsylvania Ave NW" not in deidentified_text
+    assert "Not personally identifying. Totally OK text." in deidentified_text
+
+
 def test_tyrer_cuzick_safe():
     text = """
     Risk Assessment
@@ -98,3 +143,18 @@ def test_tyrer_cuzick_safe():
     assert "Tyrer Cuzick Lifetime model risk: 3.1%" in deidentified_text
     assert "TC8 risk calculated using BI-RADS ATLAS Density b" in deidentified_text
     assert "MRS Risk Manager: No High Risk calculations" in deidentified_text
+
+
+def test_measurement_safe():
+    """
+    This string fooled the measurement_safe regex because it thought MG
+    was a unit (milligrams). I think "MG Mammo" is not a real thing, but
+    it's easy to change the rule to use a space rather than any white space
+    between a measurement and its units.
+    """
+    text = "PATIENT_ACCOUNT_NUMBER:  7590-123-987\nMG Mammo 2D/3D High Def."
+    include_map, exclude_map, data_tracker = philter.detect_phi(text, patterns=filters)
+    deidentified_text = philter.transform_text_asterisk(text, include_map)
+    assert "7590" not in deidentified_text
+    assert "123" not in deidentified_text
+    assert "987" not in deidentified_text
